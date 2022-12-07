bd81789c2495695e1f67195641adcc46f19a66ec
Joel Mathew <mail@joelmath.io>
2022-12-07 14:01:55 -0800

    Fix context menu positioning

--- web/ephesus/blueprints/word_checker/static/word_checker.js
+++ web/ephesus/blueprints/word_checker/static/word_checker.js
@@ -1,7 +1,5 @@
-
-
 async function getSpellingSuggestions() {
-  let url = 'api/v1/spell-checker';
+  let url = "api/v1/spell-checker";
 
   try {
     let res = await fetch(url);
@@ -12,81 +10,130 @@ async function getSpellingSuggestions() {
 }
 
 function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
-function underlineText(text, tokens, color="red") {
+function underlineText(text, tokens, color = "red") {
   escaped_tokens = escapeRegExp(tokens);
-  const regexp = new RegExp('\\b'+`${escaped_tokens}` + '\\b', 'gi');
+  const regexp = new RegExp("\\b" + `${escaped_tokens}` + "\\b", "gi");
   let matches = text.matchAll(regexp);
-  let underlinedText = '';
+  let underlinedText = "";
   let startIndices = Array.from(matches, (match) => match.index);
 
   if (startIndices.length == 0 || startIndices.includes(undefined)) {
-    return text;
+    return undefined;
   }
 
+  const original_tokens = text.substring(
+    startIndices[0],
+    startIndices[0] + tokens.length
+  );
 
-  const original_tokens = text.substring(startIndices[0], startIndices[0]+tokens.length);
-
-  for (let j=0; j<startIndices.length; j++) {
-    if (j===startIndices.length-1) {
-      underlinedText += '<span class="underline ' + `${color}` + '">' + original_tokens + '</span>' + text.substring(startIndices[j]+tokens.length);
+  for (let j = 0; j < startIndices.length; j++) {
+    if (j === startIndices.length - 1) {
+      underlinedText +=
+        '<span class="underline ' +
+        `${color}` +
+        '">' +
+        original_tokens +
+        "</span>" +
+        text.substring(startIndices[j] + tokens.length);
     } else {
-      underlinedText += '<span class="underline ' + `${color}` + '">' + original_tokens + '</span>' + text.substring(startIndices[j]+tokens.length, startIndices[j+1]);
+      underlinedText +=
+        '<span class="underline ' +
+        `${color}` +
+        '">' +
+        original_tokens +
+        "</span>" +
+        text.substring(startIndices[j] + tokens.length, startIndices[j + 1]);
     }
   }
   underlinedText = text.substring(0, startIndices[0]) + underlinedText;
   return underlinedText;
 }
 
-// Taken from https://htmldom.dev/show-a-custom-context-menu-at-clicked-position/
-document.addEventListener('DOMContentLoaded', function () {
-  let elements = document.getElementsByClassName("verse");
+// const ele = document.getElementById('element');
+const menu = document.getElementById("menu");
 
-  for (let i = 0; i < elements.length; i++) {
-    // console.log(elements[i].innerHTML);
+// Hide the menu when clicking outside of it
+const documentClickHandler = function (e) {
+  const isClickedOutside = !menu.contains(e.target);
+  if (isClickedOutside) {
+    menu.classList.add("hidden");
+    document.removeEventListener("click", documentClickHandler);
+  }
+};
 
-    let text = elements[i].innerText || element.textContent;
-    // underlineText(text, 'of')
-
-    elements[i].innerHTML = underlineText(text, 'jesus christ');
-
-    elements[i].addEventListener('contextmenu', function (e) {
-      e.preventDefault();
-
-      const rect = elements[i].getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      // const y = e.clientY - rect.top;
-
-      // Set the position for menu
-      menu.style.top = `${e.layerY}px`;
-      menu.style.left = `${x}px`;
-
-      // Show the menu
-      menu.classList.remove('hidden');
-
-      document.addEventListener('click', documentClickHandler);
-
-
-    });
+function addContextMenu(node, tokens) {
+  if (!(node instanceof Element)) {
+    return undefined;
   }
 
-  // const ele = document.getElementById('element');
-  const menu = document.getElementById('menu');
+  console.log(node);
+  let text = node.innerText || node.textContent;
+  if (text.toLowerCase() === tokens.toLowerCase()) {
+    node.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
 
-  // Hide the menu when clicking outside of it
-  const documentClickHandler = function (e) {
-    const isClickedOutside = !menu.contains(e.target);
-    if (isClickedOutside) {
-      menu.classList.add('hidden');
-      document.removeEventListener('click', documentClickHandler);
+      menu.style.top = `${e.layerY}px`;
+      menu.style.left = `${e.layerX}px`;
+
+      // Show the menu
+      menu.classList.remove("hidden");
+
+      document.addEventListener("click", documentClickHandler);
+    });
+  }
+}
+
+// Taken from https://htmldom.dev/show-a-custom-context-menu-at-clicked-position/
+document.addEventListener("DOMContentLoaded", function () {
+  let scriptureContent = document.getElementById("scripture-content");
+
+  // Options for the observer (which mutations to observe)
+  const observerConfig = { childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        console.log("Spotted an underline.");
+        // console.log(mutation.addedNodes[1]);
+        for (const node of mutation.addedNodes) {
+          addContextMenu(node, "jesus christ");
+          // if (node instanceof Element) {
+          //   addContextMenu(node, "jesus christ");
+          // }
+        }
+      }
     }
   };
 
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(scriptureContent, observerConfig);
+
+  // Later, you can stop observing
+  // observer.disconnect();
+
+  let verseElements = document.getElementsByClassName("verse");
+  for (let i = 0; i < verseElements.length; i++) {
+    // console.log(verseElements[i].innerHTML);
+
+    let text = verseElements[i].innerText || element.textContent;
+    // underlineText(text, 'of')
+
+    replacement = underlineText(text, "jesus christ");
+    if (replacement !== undefined) {
+      verseElements[i].innerHTML = replacement;
+    }
+  }
+
   async function Suggestions() {
     suggestions = await getSpellingSuggestions();
-    console.log(suggestions)
+    console.log(suggestions);
   }
 
   // logSuggestions();
