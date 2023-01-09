854692eb3b4dfba12f690c1ba86027a80c017512
Joel Mathew <mail@joelmath.io>
2023-01-08 22:31:50 -0800

    Add dynamic context menu

--- web/ephesus/blueprints/word_checker/static/voithos.js
+++ web/ephesus/blueprints/word_checker/static/voithos.js
@@ -25,8 +25,8 @@ async function getScriptureContent(element, formatted = true) {
 // Method to persist (POST) scriptural content to backend
 async function setScriptureContent(contentState) {
   const URL =
-    window.location.origin +
-    document.getElementsByClassName("link underline")[0].dataset.url;
+        window.location.origin +
+        document.getElementsByClassName("link underline")[0].dataset.url;
   console.log(URL);
 
   const response = await fetch(URL, {
@@ -112,14 +112,15 @@ function debounce(task, ms) {
 
 function deferred(ms) {
   let cancel,
-    promise = new Promise((resolve, reject) => {
-      cancel = reject;
-      setTimeout(resolve, ms);
-    });
+      promise = new Promise((resolve, reject) => {
+        cancel = reject;
+        setTimeout(resolve, ms);
+      });
   return { promise, cancel };
 }
 
 // Method to highlight (underline) flaggedTokens in the UI
+// And add capability to show context menu with suggestions
 function highlightTokens(suggestions) {
   let tokenPattern = undefined;
   Object.entries(suggestions).forEach(([flaggedTokenId, suggestion], index) => {
@@ -151,68 +152,131 @@ function highlightTokens(suggestions) {
       return `<span data-flagged-token-id=${tokenId.split('_').at(-1)} class="underline flag-red">${flaggedToken}</span>`
     });
   });
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Initialize state
-  let contentState = undefined;
-  let suggestions = undefined;
+  // Add context menu
+  const suggestionsMenu = document.getElementById("suggestions-menu");
+  const underlinedNodes = document.querySelectorAll("#scripture-content .underline");
+  underlinedNodes.forEach(underlinedNode => {
+    underlinedNode.addEventListener("contextmenu", menuEvent => {
+      menuEvent.preventDefault();
 
-  // Apply onclick listener for each of the resource links on the left pane
-  const resourceLinks = document.getElementsByClassName("link");
-  [].forEach.call(resourceLinks, (link) => {
-    link.addEventListener("click", (event) => {
-      // Unselect any existing and highlight selected link
-      Array.from(document.getElementsByClassName("link underline")).forEach(
-        (el) => el.classList.remove("underline")
-      );
+      suggestionsMenu.style.top = `${menuEvent.layerY}px`;
+      suggestionsMenu.style.left = `${menuEvent.layerX}px`;
 
-      // Underline selected resource link
-      event.target.classList.add("underline");
+      // Setup and show the menu
+      ////  Clean-up any existing `li` tags in the suggestions-menu
+      suggestionsMenu.querySelectorAll('li').forEach((liElement) => liElement.remove());
+      //// Add the relevant suggestions to the suggestions-menu
+      suggestions[menuEvent.target.dataset.flaggedTokenId].suggestions.forEach(suggestionItem => {
+        liElement = document.createElement('li');
+        liElement.classList.add("menu-item");
+        liElement.innerHTML = suggestionItem.suggestion;
+        suggestionsMenu.append(liElement);
+      });
 
-      // Get JSON content for internal state management
-      // Send formatted=`false` for returning JSON
-      getScriptureContent(event.target, false)
-        .then((content) => {
-          contentState = content;
-        })
-        .then(() => {
-          console.log(contentState);
-        });
+      suggestionsMenu.classList.remove("hidden");
 
-      // Get HTML scripture content to display on right pane
-      getScriptureContent(event.target)
-        .then(content => {
-          let scriptureContent = document.getElementById("scripture-content");
-          scriptureContent.innerHTML = content;
-          return getSuggestions(event.target);
-        })
-        .then(suggestionsData => {
-          suggestions = suggestionsData;
-          console.log(suggestions);
-          highlightTokens(suggestions);
-
-
-          // Apply onblur listeners for each verse for writing to backend
-          const verses = document.getElementsByClassName("verse");
-          [].forEach.call(verses, (verse) => {
-            verse.addEventListener(
-              "input",
-              debounce((event) => {
-                contentState[event.target.dataset.book][
-                  event.target.dataset.chapter
-                ][event.target.dataset.verse] = event.target.innerHTML.trim();
-
-                // Persist data to backend
-                setScriptureContent(contentState);
-              }, 3000)
-            );
-          });
-        })
-        .catch((err) => {
-          let scriptureContent = document.getElementById("scripture-content");
-          scriptureContent.innerHTML = err;
-        });
+      // Hide the suggestionsMenu when clicking outside of it
+      document.addEventListener("click", element => {
+        const isClickedOutside = !suggestionsMenu.contains(element.target);
+        if (isClickedOutside) {
+          suggestionsMenu.classList.add("hidden");
+          document.removeEventListener("click", arguments.callee);
+        }
+      });
     });
   });
-});
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Initialize state
+    let contentState = undefined;
+    let suggestions = undefined;
+
+    let scriptureContent = document.getElementById("scripture-content");
+
+    // Options for the observer (which mutations to observe)
+    const observerConfig = { characterData: true, characterDataOldValue: true, subtree: true};
+
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+      console.log(mutationList);
+      // for (const mutation of mutationList) {
+      //   if (mutation.type === "childList") {
+      //     console.log("Spotted an underline.");
+      //     // console.log(mutation.addedNodes[1]);
+      //     for (const node of mutation.addedNodes) {
+      //       addContextMenu(node, "confidint");
+      //       // if (node instanceof Element) {
+      //       //   addContextMenu(node, "jesus christ");
+      //       // }
+      //     }
+      //   }
+      //}
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+
+    // Apply onclick listener for each of the resource links on the left pane
+    const resourceLinks = document.getElementsByClassName("link");
+    [].forEach.call(resourceLinks, (link) => {
+      link.addEventListener("click", (event) => {
+        // Unselect any existing and highlight selected link
+        Array.from(document.getElementsByClassName("link underline")).forEach(
+          (el) => el.classList.remove("underline")
+        );
+
+        // Underline selected resource link
+        event.target.classList.add("underline");
+
+        // Get JSON content for internal state management
+        // Send formatted=`false` for returning JSON
+        getScriptureContent(event.target, false)
+          .then((content) => {
+            contentState = content;
+          })
+          .then(() => {
+            console.log(contentState);
+          });
+
+        // Get HTML scripture content to display on right pane
+        getScriptureContent(event.target)
+          .then(content => {
+            scriptureContent.innerHTML = content;
+
+            // Start observing the target node for configured mutations
+            observer.observe(scriptureContent, observerConfig);
+
+            return getSuggestions(event.target);
+          })
+          .then(suggestionsData => {
+            suggestions = suggestionsData;
+            console.log(suggestions);
+            highlightTokens(suggestions);
+
+
+            // Apply onblur listeners for each verse for writing to backend
+            const verses = document.getElementsByClassName("verse");
+            [].forEach.call(verses, (verse) => {
+              verse.addEventListener(
+                "input",
+                debounce((event) => {
+                  contentState[event.target.dataset.book][
+                    event.target.dataset.chapter
+                  ][event.target.dataset.verse] = event.target.innerHTML.trim();
+
+                  // Persist data to backend
+                  setScriptureContent(contentState);
+                }, 3000)
+              );
+            });
+          })
+          .catch((err) => {
+            scriptureContent.innerHTML = err;
+          });
+      });
+    });
+  });
