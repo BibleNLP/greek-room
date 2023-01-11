ecb49eb0510e75f39e6b6c6f4ec031f8903979ba
Joel Mathew <mail@joelmath.io>
2023-01-10 17:58:45 -0800

    Clean-up memory leak

--- web/ephesus/blueprints/word_checker/static/voithos.js
+++ web/ephesus/blueprints/word_checker/static/voithos.js
@@ -1,19 +1,7 @@
 // Javascript for the Voithos page
 
-function getCursorPosition(editor) {
-  if (window.getSelection()) {
-    var sel = window.getSelection();
-    if (sel.getRangeAt) {
-      var pos = sel.getRangeAt(0).startOffset;
-      var endPos =
-        pos +
-        Array.from(editor.innerHTML.slice(0, pos)).length -
-        editor.innerHTML.slice(0, pos).split("").length;
-      return endPos;
-    }
-  }
-  return null;
-}
+import { Cursor } from "./utils.js";
+import { debounce } from "./utils.js";
 
 // Method to get formatted scripture content from the backend
 async function getScriptureContent(element, formatted = true) {
@@ -24,14 +12,14 @@ async function getScriptureContent(element, formatted = true) {
 
   const response = await fetch(URL);
   if (response.ok) {
-    let body = undefined;
+    let data = undefined;
 
     if (formatted) {
-      body = await response.text();
+      data = await response.text();
     } else {
-      body = await response.json();
+      data = await response.json();
     }
-    return Promise.resolve(body);
+    return Promise.resolve(data);
   } else {
     return Promise.reject("Unable to retrieve data.");
   }
@@ -42,7 +30,6 @@ async function setScriptureContent(contentState) {
   const URL =
     window.location.origin +
     document.getElementsByClassName("link underline")[0].dataset.url;
-  console.log(URL);
 
   const response = await fetch(URL, {
     method: "POST",
@@ -53,8 +40,8 @@ async function setScriptureContent(contentState) {
   });
 
   if (response.ok) {
-    body = await response.json();
-    return Promise.resolve(body);
+    let data = await response.json();
+    return Promise.resolve(data);
   } else {
     return Promise.reject("Error while persisting data.");
   }
@@ -100,40 +87,16 @@ async function getSuggestions(resourceId, filters) {
 
   const response = await fetch(URL);
   if (response.ok) {
-    let body = await response.json();
-    return Promise.resolve(body);
+    let data = await response.json();
+    return Promise.resolve(data);
   } else {
     return Promise.reject("Unable to retrieve data.");
   }
 }
 
-// Debounce input
-function debounce(task, ms) {
-  let t = { promise: null, cancel: (_) => void 0 };
-  return async (...args) => {
-    try {
-      t.cancel();
-      t = deferred(ms);
-      await t.promise;
-      await task(...args);
-    } catch (_) {
-      //console.log("cleaning up cancelled promise");
-    }
-  };
-}
-
-function deferred(ms) {
-  let cancel,
-    promise = new Promise((resolve, reject) => {
-      cancel = reject;
-      setTimeout(resolve, ms);
-    });
-  return { promise, cancel };
-}
-
 // Method to highlight (underline) flaggedTokens in the UI
 // and add capability to show context menu with suggestions
-function highlightTokens(suggestions, contentState) {
+function highlightTokens(suggestions, contentState, isRestoreCursor = false) {
   let tokenPattern = undefined;
   Object.entries(suggestions).forEach(([flaggedTokenId, suggestion], index) => {
     // The regex is a concatenation of all flaggedTokens
@@ -148,10 +111,21 @@ function highlightTokens(suggestions, contentState) {
       tokenPattern += `|\\b(?<index_${flaggedTokenId}>${suggestion.flaggedToken})\\b`;
     }
   });
-  // console.log(tokenPattern);
-  tokenRegExp = new RegExp(tokenPattern, "ig");
+  const tokenRegExp = new RegExp(tokenPattern, "ig");
 
   const verses = document.getElementsByClassName("verse");
+
+  // Logic to keep the cursor in the same place
+  let offset = undefined;
+  let anchorVerseElement = undefined;
+  if (isRestoreCursor && window.getSelection().type !== "None") {
+    anchorVerseElement = window
+      .getSelection()
+      .getRangeAt(0)
+      .startContainer.parentElement.closest("span.verse");
+    offset = Cursor.getCurrentCursorPosition(anchorVerseElement);
+  }
+
   [].forEach.call(verses, (verseElement) => {
     // This function gets called repeatedly for
     // every successful match
@@ -162,7 +136,7 @@ function highlightTokens(suggestions, contentState) {
         // Get last element which is an object
         // with named capture groups: args.pop()
         // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_function_as_the_replacement
-        [tokenId, flaggedToken] = Object.entries(args.pop()).find(
+        const [tokenId, flaggedToken] = Object.entries(args.pop()).find(
           ([tokenId, flaggedToken]) => flaggedToken !== undefined
         );
         return `<span data-flagged-token-id=${tokenId
@@ -170,6 +144,11 @@ function highlightTokens(suggestions, contentState) {
           .at(-1)} class="underline flag-red">${flaggedToken}</span>`;
       });
   });
+
+  if (isRestoreCursor && offset !== undefined) {
+    Cursor.setCurrentCursorPosition(offset, anchorVerseElement);
+    anchorVerseElement.focus();
+  }
 
   // Add context menu
   const suggestionsMenu = document.getElementById("suggestions-menu");
@@ -184,14 +163,14 @@ function highlightTokens(suggestions, contentState) {
       suggestionsMenu.style.left = `${menuEvent.layerX}px`;
 
       // Setup and show the menu
-      ////  Clean-up any existing `li` tags in the suggestions-menu
+      // Clean-up any existing `li` tags in the suggestions-menu
       suggestionsMenu
         .querySelectorAll("li")
         .forEach((liElement) => liElement.remove());
-      //// Add the relevant suggestions to the suggestions-menu
+      // Add the relevant suggestions to the suggestions-menu
       suggestions[menuEvent.target.dataset.flaggedTokenId].suggestions.forEach(
         (suggestionItem) => {
-          liElement = document.createElement("li");
+          const liElement = document.createElement("li");
           liElement.classList.add("menu-item");
           liElement.innerHTML = suggestionItem.suggestion;
           suggestionsMenu.append(liElement);
@@ -218,12 +197,13 @@ document.addEventListener("DOMContentLoaded", () => {
   let suggestions = undefined;
   let resourceId = undefined;
 
-  // Used to hold the chapters and verses for
-  // the content that were edited by the user
+  // Used to hold the chapters for the
+  // content that was edited by the user
   let dirtyState = undefined;
 
   let scriptureContent = document.getElementById("scripture-content");
 
+  // MutationObserver setup
   // Options for the observer (which mutations to observe)
   const observerConfig = {
     characterData: true,
@@ -232,12 +212,8 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   // Callback function to execute when mutations are observed
-  const callback = (mutationList, observer) => {
-    console.log(mutationList);
+  const getDirtyState = (mutationList, observer) => {
     mutationList.forEach((mutation) => {
-      console.log(
-        getCursorPosition(mutation.target.parentElement.closest("span.verse"))
-      );
       // Find the closest verse span to get
       // current chapter and verse number
       const { book, chapter, verse } =
@@ -248,22 +224,10 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       dirtyState[book].chapters.add(chapter);
     });
-    // for (const mutation of mutationList) {
-    //   if (mutation.type === "childList") {
-    //     console.log("Spotted an underline.");
-    //     // console.log(mutation.addedNodes[1]);
-    //     for (const node of mutation.addedNodes) {
-    //       addContextMenu(node, "confidint");
-    //       // if (node instanceof Element) {
-    //       //   addContextMenu(node, "jesus christ");
-    //       // }
-    //     }
-    //   }
-    //}
   };
 
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(callback);
+  // Create an observer instance
+  const observer = new MutationObserver(getDirtyState);
 
   // Apply onclick listener for each of the resource links on the left pane
   const resourceLinks = document.getElementsByClassName("link");
@@ -282,59 +246,66 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Get JSON content for internal state management
       // Send formatted=`false` for returning JSON
-      getScriptureContent(event.target, false)
-        .then((content) => {
-          contentState = content;
-        })
-        .then(() => {
-          console.log(contentState);
-        });
+      getScriptureContent(event.target, false).then((content) => {
+        contentState = content;
+      });
 
       // Get HTML scripture content to display on right pane
       getScriptureContent(event.target)
         .then((content) => {
           scriptureContent.innerHTML = content;
 
-          // Start observing the target node for configured mutations
+          // Start observing the content for text edits
           observer.observe(scriptureContent, observerConfig);
 
+          // Get the suggestions for the text
           return getSuggestions(resourceId);
         })
         .then((suggestionsData) => {
+          // Set state
           suggestions = suggestionsData;
-          console.log(suggestions);
           highlightTokens(suggestions, contentState);
 
-          // Apply onblur listeners for each verse for writing to backend
+          // Apply onblur listeners for writing to backend
+          // scriptureContent.removeEventListener(
+          //   "input",
+          //   debounce(callback(event), 3000)
+          // );
 
-          document.getElementById("scripture-content").addEventListener(
-            "input",
-            debounce((event) => {
-              const verses = document.getElementsByClassName("verse");
-              [].forEach.call(verses, (verse) => {
-                contentState[verse.dataset.book][verse.dataset.chapter][
-                  verse.dataset.verse
-                ] = verse.innerText.trim();
-              });
+          if (!scriptureContent.hasAttribute("inputListener")) {
+            scriptureContent.addEventListener(
+              "input",
+              debounce((event) => {
+                const verses = document.getElementsByClassName("verse");
+                [].forEach.call(verses, (verse) => {
+                  contentState[verse.dataset.book][verse.dataset.chapter][
+                    verse.dataset.verse
+                  ] = verse.innerText.trim();
+                });
 
-              // Persist data to backend
-              setScriptureContent(contentState);
+                // Persist edited data to backend
+                setScriptureContent(contentState);
 
-              getSuggestions(resourceId, dirtyState).then(
-                (updatedSuggestionsData) => {
-                  suggestions = {
-                    ...suggestionsData,
-                    ...updatedSuggestionsData,
-                  };
-                  console.log(suggestions);
-                  highlightTokens(suggestions, contentState);
+                // Get updated suggestions
+                getSuggestions(resourceId, dirtyState).then(
+                  (updatedSuggestions) => {
+                    // Merge in the updated
+                    // suggestions into current state
+                    suggestions = {
+                      ...suggestions,
+                      ...updatedSuggestions,
+                    };
+                    // `true` to restore cursor position after highlighting
+                    highlightTokens(suggestions, contentState, true);
 
-                  // Reset dirtyState
-                  dirtyState = undefined;
-                }
-              );
-            }, 3000)
-          );
+                    // Reset dirtyState
+                    dirtyState = undefined;
+                  }
+                );
+              }, 3000)
+            );
+            scriptureContent.setAttribute("inputListener", "true");
+          }
         })
         .catch((err) => {
           scriptureContent.innerHTML = err;
