57e1780fb404da14ab74f3347ed5a22d13d76bb9
Joel Mathew <mail@joelmath.io>
2023-01-23 22:31:20 -0800

    Fix initial setup

--- web/ephesus/model/seed.py
+++ web/ephesus/model/seed.py
@@ -14,14 +14,24 @@ from web.ephesus.model.voithos import (
 seed_data = [
     {
         "flaggedTokens": {"lang_code": "eng", "token": "confidint"},
-        "suggestion": {
-            "lang_code": "eng",
-            "suggestion": "confident",
-            "suggestion_type": SuggestionType.SPELLING,
-            "confidence": 0.7,
-            "user_decision": UserDecision.UNDECIDED,
-            "suggestion_source": SuggestionSource.AI,
-        },
+        "suggestions": [
+            {
+                "lang_code": "eng",
+                "suggestion": "confident",
+                "suggestion_type": SuggestionType.SPELLING,
+                "confidence": 0.7,
+                "user_decision": UserDecision.UNDECIDED,
+                "suggestion_source": SuggestionSource.AI,
+            },
+            {
+                "lang_code": "eng",
+                "suggestion": "confidante",
+                "suggestion_type": SuggestionType.SPELLING,
+                "confidence": 0.3,
+                "user_decision": UserDecision.UNDECIDED,
+                "suggestion_source": SuggestionSource.AI,
+            },
+        ],
     }
 ]
 
@@ -30,10 +40,12 @@ app = ephesus_app.create_app()
 with app.app_context():
     for item in seed_data:
         flaggedTokens = FlaggedTokens(**item["flaggedTokens"])
-        suggestions = Suggestions(**item["suggestion"])
-        flaggedTokens.suggestions.append(suggestions)
+        # suggestions = Suggestions(**item["suggestion"])
+        for suggestion_item in item["suggestions"]:
+            suggestion = Suggestions(**suggestion_item)
+            flaggedTokens.suggestions.append(suggestion)
 
+            db.session.add(suggestion)
         db.session.add(flaggedTokens)
-        db.session.add(suggestions)
 
     db.session.commit()
