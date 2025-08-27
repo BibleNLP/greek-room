66517ab6e332804901ab271d471876eec2b549cf
Ulf Hermjakob 2 <ulf2@beethoven.local>
2025-08-26 17:55:18 -0700

    updated version

--- greekroom/__init__.py
+++ greekroom/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __description__ = '''The Greek Room will be a suite of tools supporting Biblical natural language processing.'''
 last_mod_date = 'August 26, 2025'
