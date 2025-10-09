79bbbe81d7adafad6a3c53a6ffdcae5f3dc81416
Ulf Hermjakob <ulfhermjakob@gmail.com>
2025-10-08 17:56:55 -0700

    added NumberStyle section

--- greekroom/greekroom/gr_utilities/wb_file_props.py
+++ greekroom/greekroom/gr_utilities/wb_file_props.py
@@ -117,6 +117,7 @@ class PunctStyle:
         self.pair_counts = defaultdict(lambda: defaultdict(int))
         self.quotation_mark_characters = """‘’‚"“”„«»‹›"""
         self.quotation_mark_characters2 = self.quotation_mark_characters + "'ʼ"
+        self.quotation_mark_characters2 += '\u00AD'   # soft hyphen
         self.quotation_pair_candidates = [
             ['“', '”'], ['„', '”'], ['”', '”'], ['“', '“'], ['„', '“'], ['«', '»'], ['‹', '›'],
             ["‘", "’"], ["‚", "’"], ["’", "’"]]
@@ -231,34 +232,152 @@ class PunctStyle:
                         sep = "…"
                         f_html.write(f"          <tr><td>{c1}</td><td>{sep}</td><td>{c2}</td><td>&nbsp;&nbsp;</td>"
                                      f"<td>{name1}</td><td>{sep}</td><td>{name2}</td><td>&nbsp;&nbsp;</td>"
-                                     f"<td>{count}</td></tr>\n")
+                                     f"<td align='right'>{count}</td></tr>\n")
                 f_html.write("          </table>\n")
             f_html.write("      </ul>\n")
 
 
-def script_punct(input_filename: str | None = None, input_string: str | None = None,
+class NumberStyle:
+    """Initialized with a copy from ualign.py"""
+    def __init__(self, text: str | None = None):
+        self.digital_num_pattern_counts = defaultdict(int)
+        self.digital_num_pattern_examples = defaultdict(list)
+        self.decimal_grouping = None
+        self.decimal_separator = None
+        self.digit_group_separator = None
+        if text:
+            self.add_stats(text)
+
+    def add_stats(self, text: str, _verbose: bool = False) -> None:
+        for complex_num_s in regex.findall(r'(?:[-])?\d+(?:(?:\pP|\pS|\pZ)+\d+)*', text):
+            num_pattern = regex.sub(r'\d', 'D', complex_num_s)
+            self.digital_num_pattern_counts[num_pattern] += 1
+            examples = self.digital_num_pattern_examples[num_pattern]
+            if complex_num_s not in examples:
+                max_n_examples = 10
+                if len(examples) < max_n_examples:
+                    self.digital_num_pattern_examples[num_pattern].append(complex_num_s)
+                elif len(examples) == max_n_examples:
+                    self.digital_num_pattern_examples[num_pattern].append('…')
+
+    @staticmethod
+    def key_with_exclusive_highest_count(d: dict) -> str | None:
+        highest_count = 0
+        result = None
+        for k in d.keys():
+            count = d.get(k, 0)
+            if count > highest_count:
+                result = k
+                highest_count = count
+            elif count == highest_count:
+                result = None
+        return result
+
+    def add_conclusions(self):
+        number_style_count = defaultdict(int)  # "Western", "Chinese", "Indian"
+        digit_group_separator_counts = defaultdict(int)
+        decimal_separator_counts = defaultdict(int)
+        for num_pattern in self.digital_num_pattern_counts:
+            num_pattern_count = self.digital_num_pattern_counts[num_pattern]
+            for digit_group_separator in (',', '.', '\u202F'):  # non-breakable narrow space
+                g_digit_group_separator = ('\\' if digit_group_separator in ('.',) else '') + digit_group_separator
+                match_p = False
+                if regex.match(fr'DD?D?(?:{g_digit_group_separator}DDD)+$', num_pattern):
+                    number_style_count["Western"] += num_pattern_count
+                    match_p = True
+                if regex.match(fr'DD?(?:{g_digit_group_separator}DD)*{g_digit_group_separator}DDD$', num_pattern):
+                    number_style_count["Indian"] += num_pattern_count
+                    match_p = True
+                if regex.match(fr'DD?D?D?(?:{g_digit_group_separator}DDDD)+$', num_pattern):
+                    number_style_count["Chinese"] += num_pattern_count
+                    match_p = True
+                if match_p:
+                    digit_group_separator_counts[digit_group_separator] += num_pattern_count
+            for decimal_separator in (',', '.'):
+                g_decimal_separator = ('\\' if decimal_separator in ('.',) else '') + decimal_separator
+                if regex.match(fr'DD?D?{g_decimal_separator}DD?$', num_pattern):
+                    decimal_separator_counts[decimal_separator] += num_pattern_count
+        self.decimal_grouping = self.key_with_exclusive_highest_count(number_style_count)
+        self.decimal_separator = self.key_with_exclusive_highest_count(decimal_separator_counts)
+        self.digit_group_separator = self.key_with_exclusive_highest_count(digit_group_separator_counts)
+        # sys.stderr.write(f"R S:{number_style_count} G:{digit_group_separator_counts} D:{decimal_separator_counts}\n")
+        # sys.stderr.write(f"F S:{self.decimal_grouping} G:{self.digit_group_separator} D:{self.decimal_separator}\n")
+
+    @staticmethod
+    def separator_class(digital_num_pattern: str) -> str:
+        separator_list = []
+        for separator in regex.findall(r'[^D]+', digital_num_pattern):
+            s = ''
+            for c in separator:
+                ud_name = ud.name(c)
+                s += f"<{ud_name}>" if ud_name else c
+            if s not in separator_list:
+                separator_list.append(s)
+        return "   ".join(separator_list)
+
+    @staticmethod
+    def print_to_html(d: dict, f_html: TextIO) -> None:
+        if d:
+            f_html.write(f"    <li> Digital number style\n")
+            f_html.write("      <ul>\n")
+            if style_d := d.get('style'):
+                f_html.write(f"      <li> Style: {style_d}\n")
+            if count_d := d.get('counts'):
+                examples_d = d.get('examples')
+                f_html.write(f"      <li> Counts"
+                             f" &nbsp;&nbsp; (<i>D</i> stands for a digit)<br>\n")
+                f_html.write(f"   <table><tr><th>Digital pattern</th><th>Separators</th>"
+                             f"<th>Count</th>"
+                             f"<td align='left'>&nbsp;&nbsp;&nbsp;&nbsp;<b>Examples</b> (unique)</td></tr>\n")
+                for num_pattern in sorted(count_d, key=lambda k: (NumberStyle.separator_class(k), len(k))):
+                    sep = NumberStyle.separator_class(num_pattern)
+                    g_sep = html_util.guard_html(sep).replace('  ', ' &nbsp; ')
+                    examples = " &nbsp; ".join(examples_d.get(num_pattern))
+                    f_html.write(f"   <tr><td align='right'>{num_pattern}</td>"
+                                 f"<td align='center'>&nbsp;&nbsp;{g_sep}</td>"
+                                 f"<td align='right'>{count_d[num_pattern]}</td>"
+                                 f"<td>&nbsp;&nbsp;&nbsp;&nbsp;{examples}</td></tr>\n")
+                f_html.write("        </table>\n")
+            f_html.write("      </ul>\n")
+
+
+def script_props(input_filename: str | None = None, input_string: str | None = None,
                  lang_code: str | None = None, lang_name: str | None = None)\
         -> dict:
     script_direction = ScriptDirection(lang_code, lang_name)
     punct_style = PunctStyle()
+    number_style = NumberStyle()
     if input_filename:
         with open(input_filename) as f:
             for line in f:
                 punct_style.add_stats(line)
+                number_style.add_stats(line)
                 for c in line:
                     script_direction.add_stats(c, 1)
     if input_string:
         punct_style.add_stats(input_string)
+        number_style.add_stats(input_string)
         for c in input_string:
             script_direction.add_stats(c, 1)
     punct_style.add_conclusions()
-    report = script_direction.make_report(True)
+    number_style.add_conclusions()
+    script_report = script_direction.make_report(True)
+    constructed_digital_num_style_dict = {}
+    if number_style.decimal_grouping:
+        constructed_digital_num_style_dict["decimal-grouping"] = number_style.decimal_grouping
+    if number_style.decimal_separator:
+        constructed_digital_num_style_dict["decimal-separator"] = number_style.decimal_separator
+    if number_style.digit_group_separator:
+        constructed_digital_num_style_dict["digit-group-separator"] = number_style.digit_group_separator
     result = {"script-direction": {"direction": script_direction.determine_direction(),
                                    "counts": script_direction.bidirectional_class_counts,
-                                   "report": report},
+                                   "report": script_report},
               "punct-style": {"quotation-pairs": punct_style.quotation_pairs,
                               "counts": punct_style.d,
                               "pair-counts": punct_style.pair_counts},
+              "number-style": {"style": constructed_digital_num_style_dict,
+                               "counts": number_style.digital_num_pattern_counts,
+                               "examples": number_style.digital_num_pattern_examples},
               "n-chars": punct_style.n_chars}
     if lang_code:
         result["lang-code"] = lang_code
@@ -285,6 +404,7 @@ def print_to_html(d: dict, f_html: TextIO) -> None:
     print_meta_to_html(d.get("lang-code"), d.get("lang-name"), f_html)
     ScriptDirection.print_to_html(d.get("script-direction"), f_html)
     PunctStyle.print_to_html(d.get("punct-style"), f_html)
+    NumberStyle.print_to_html(d.get("number-style"),  f_html)
     f_html.write("  </ul>\n")
     html_util.print_html_foot(f_html)
 
@@ -318,7 +438,7 @@ def main():
         json_out_filename = f"{owl_folder}/props.json"
         html_out_filename = f"{owl_folder}/props.html"
 
-    d = script_punct(args.input_filename, args.input_string, lang_code, lang_name)
+    d = script_props(args.input_filename, args.input_string, lang_code, lang_name)
     if json_out_filename:
         with open(json_out_filename, "w") as f_json:
             f_json.write(f"{json.dumps(d)}\n")
