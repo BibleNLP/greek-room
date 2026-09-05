84b4d7e326e38bfcce08d852d7d001b0d31014b1
Ulf Hermjakob <ulfhermjakob@gmail.com>
2026-09-05 19:29:11 +0200

    update version number

--- greekroom/greekroom/__init__.py
+++ greekroom/greekroom/__init__.py
@@ -1,7 +1,7 @@
 r"""Greek Room is a collection of tools to support Bible translators with quality control."""
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __formatVersion__ = '0.0.4'
 __description__ = '''Greek Room is a collection of tools to support Bible translators with quality control.'''
-last_mod_date = 'August 06, 2026'
+last_mod_date = 'September 05, 2026'
 
