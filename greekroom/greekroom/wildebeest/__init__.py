961b6861a618cb91a6c640ddd1bb650e293aad56
Ulf Hermjakob <ulfhermjakob@gmail.com>
2026-09-08 15:04:01 +0200

    commented out imports

--- greekroom/greekroom/wildebeest/__init__.py
+++ greekroom/greekroom/wildebeest/__init__.py
@@ -5,13 +5,15 @@ mapping digits and some punctuation to ASCII, deletion of some non-printable cha
 Wildebeest documentation: https://github.com/uhermjakob/wildebeest
 Main modules: wildebeest.wb_analysis, wildebeest.wb_normalize
 Argument help: wb_analysis.py -h, wb_normalize.py -h; or, alternatively: wb-ana -h, wb-norm -h"""
-__version__ = '0.11.3'
+
+__version__ = '0.11.4'
 __description__ = '''The Wildebeest scripts investigate, repair and normalize a wide range of text file problems
 at the character level, e.g. encoding errors, normalization of characters into their canonical form,
 mapping digits and some punctuation to ASCII, deletion of some non-printable characters.'''
-last_mod_date = 'September 05, 2026'
-from . import wb_normalize
-from . import wb_analysis
-from . import wb_check
-# from . import utilities
-__all__ = [wb_normalize, wb_analysis, wb_check]
+last_mod_date = 'September 08, 2026'
+
+# from __future__ import annotations
+# from . import wb_normalize
+# from . import wb_analysis
+# from . import wb_check
+# __all__ = [wb_normalize, wb_analysis, wb_check]
