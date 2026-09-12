c78c3577df6aa141f8767e5576251f78b03bdf5a
Ulf Hermjakob <ulfhermjakob@gmail.com>
2026-09-12 15:15:56 +0200

    update version number

--- greekroom/greekroom/wildebeest/__init__.py
+++ greekroom/greekroom/wildebeest/__init__.py
@@ -6,7 +6,7 @@ Wildebeest documentation: https://github.com/uhermjakob/wildebeest
 Main modules: wildebeest.wb_analysis, wildebeest.wb_normalize
 Argument help: wb_analysis.py -h, wb_normalize.py -h; or, alternatively: wb-ana -h, wb-norm -h"""
 
-__version__ = '0.11.4'
+__version__ = '0.11.5'
 __description__ = '''The Wildebeest scripts investigate, repair and normalize a wide range of text file problems
 at the character level, e.g. encoding errors, normalization of characters into their canonical form,
 mapping digits and some punctuation to ASCII, deletion of some non-printable characters.'''
